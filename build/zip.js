ddcdce8cd4f499a94ec946947c95face7f142f23
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2020-12-10 14:36:01 -0800

    Add icons to zip files (#3)

--- build/zip.js
+++ build/zip.js
@@ -4,6 +4,12 @@ const yazl = require('yazl')
 
 const CONTENT_SCRIPT = 'contentscript.js'
 const MANIFEST = 'manifest.json'
+const IMAGES = 'images'
+const ICONS = [
+  'icon-16.png',
+  'icon-48.png',
+  'icon-128.png',
+]
 
 module.exports = function zip (platformName, unpackedPath, packedPath) {
   return new Promise((resolve) => {
@@ -17,6 +23,12 @@ module.exports = function zip (platformName, unpackedPath, packedPath) {
       path.resolve(unpackedPath, platformName, MANIFEST),
       MANIFEST,
     )
+    for (const icon of ICONS) {
+      zipFile.addFile(
+        path.resolve(unpackedPath, platformName, IMAGES, icon),
+        `${IMAGES}/${icon}`,
+      )
+    }
     zipFile.end()
 
     zipFile.outputStream
